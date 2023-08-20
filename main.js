314f99f430862dfde7a155237822597fa0a8e05a
Leroy Mogire <ntruderleroy@gmail.com>
2023-08-20 17:37:04 +0300

    form validation
    form validation done with javascript

--- main.js
+++ main.js
@@ -1,56 +1,30 @@
 const form = document.querySelector('form');
-const firstNombre = document.querySelector('.first');
-const lastNombre = document.querySelector('.last');
-const email = document.querySelector('.email');
-const password = document.querySelector('.password');
-const firstError = document.querySelector('.error1');
-const lastError = document.querySelector(".error2");
-const emailError = document.querySelector(".error3");
-const pswdError = document.querySelector(".error4");
+const inputs = document.querySelectorAll('input')
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const firstName = firstNombre.value.trim();
-    const lastName = lastNombre.value.trim();
-    const address = email.value.trim();
-    const pswd = password.value.trim();
+    e.preventDefault()
 
-    const validateEmail = (address) => {
-      return String(email)
-        .toLowerCase()
-        .match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-    };
+    inputs.forEach((input) => {
+        if (!input.value) {
+            input.parentElement.classList.add('error')
+        }   else {
+            input.parentElement.classList.remove('error')
+        }
 
-    if (firstName === '') {
-        console.log('first name is empty');
-        firstError.textContent = 'first name cannot empty';
-    }   else {
-        console.log ('first name is good');
-    }
-    
-    if (lastName === "") {
-      console.log("last name is empty");
-      lastError.textContent = "last name cannot empty";
-    } else {
-      console.log("last name is good");
-    }
-
-    if (address === '') {
-        console.log('email field is empty');
-        emailError.textContent = "email field cannot empty";
-    }   else if (!validateEmail(address)) {
-        console.log('wrong email format')
-        emailError.textContent = 'Looks like this is not an email';
-    }   else {
-        console.log('email is good');
-    }
-
-    if (pswd === '') {
-        console.log('password cannot be empty');
-        pswdError.textContent = "password cannot be empty";
-    }   else {
-        console.log('password is good');
-    }
+        if (input.type == 'email') {
+            if (validateEmail(input.value)) {
+                input.parentElement.classList.remove("error");
+            }   else {
+                input.parentElement.classList.add("error");
+            }
+        }
+    })
 })
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
